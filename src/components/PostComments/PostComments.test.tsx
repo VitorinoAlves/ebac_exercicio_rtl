580dd8000ed9b820c4585cc4f17db88b8a28907f
VitorinoAlves <vitorino.17alexandre@gmail.com>
2025-02-10 05:38:15 -0300

    Adicionando novos testes unitarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,18 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    
+    it('Deve adicionr dois comentários', () => {
+        render(<PostComment/>);
+        const commentField = screen.getByTestId('comment-field');
+        const btnComentar = screen.getByTestId('btn-comentar');
+
+        fireEvent.change(commentField, {target: {value: 'Comentário teste 01'}});
+        fireEvent.click(btnComentar);
+        fireEvent.change(commentField, {target: {value: 'Comentário teste 02'}});
+        fireEvent.click(btnComentar);
+
+        expect(screen.getByText('Comentário teste 01')).toBeInTheDocument();
+        expect(screen.getByText('Comentário teste 02')).toBeInTheDocument();
+    })
 });
